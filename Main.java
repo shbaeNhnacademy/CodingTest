238f6e2937fc07b2a8ad476fd91f4b76c417ea36
m2_SH <shbae4501@gmail.com>
2022-11-08 00:51:09 +0900

    3번째 시도 - BufferedReader 사용해봤지만 시간초과

--- Main.java
+++ Main.java
@@ -1,20 +1,27 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
+    private static String space = " ";
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str1 = br.readLine().split(space);
+
+        int n = Integer.parseInt(str1[0]);
+        int m = Integer.parseInt(str1[1]);
+
         int[] ints = new int[n];
-        for (int i = 0; i < n; i++) {
-            ints[i] = scanner.nextInt();
+        String[] str2 = br.readLine().split(space);
+
+        for (int i = 0; i < str2.length; i++) {
+            ints[i] = Integer.parseInt(str2[i]);
         }
         
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < m; i++) {
-            int start = scanner.nextInt() - 1;
-            int end = scanner.nextInt() - 1;
+            String[] str3 = br.readLine().split(space);
+            int start = Integer.parseInt(str3[0]) - 1;
+            int end = Integer.parseInt(str3[1]) - 1;
             int sum = 0;
             for (int j = start; j <= end; j++) {
                 sum += ints[j];
